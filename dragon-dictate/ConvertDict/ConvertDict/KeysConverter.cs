35bc1746f21b873e101cc4e6cc89035dae8ba3d9
ronzulu <75528127+ronzulu@users.noreply.github.com>
2024-10-02 10:38:39 +1000

    Progress

--- dragon-dictate/ConvertDict/ConvertDict/KeysConverter.cs
+++ dragon-dictate/ConvertDict/ConvertDict/KeysConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,40 @@ namespace ConvertDict
 {
     public class KeysConverter
     {
+        public static string Convert(string ddKeys)
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (i < ddKeys.Length)
+            {
+                char c = ddKeys[i];
+                if (c == '{')
+                {
+                    int end = ddKeys.IndexOf('}', i);
+                    if (end == -1)
+                        sb.Append(c);
+                    else
+                    {
+                        int length = end - i + 1;
+                        string virtualKey = ddKeys.Substring(i, length);
+                        sb.Append(ConvertSingleKey(virtualKey));
+                        i = end;
+                    }
+                }
+                else
+                    sb.Append(c);
+
+                i++;
+            }
+            return sb.ToString();
+        }
+
+        public static string ConvertSingleKey(string virtualKey)
+        {
+            string str = virtualKey.ToLower();
+            str = str.Replace("ext", "");
+            str = str.Replace("+", "-");
+            return str;
+        }
     }
 }
---
+++ dragon-dictate/ConvertDict/TestProject/KeysConverterTest.cs
@@ -0,0 +1,68 @@
+﻿using ConvertDict;
+using Microsoft.VisualStudio.CodeCoverage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class KeysConverterTest
+    {
+        private static char q = '"';
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            TestSingle("{Enter}", "{enter}");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            TestSingle("{Ctrl+v}", "{ctrl-v}");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            TestSingle("{Ctrl+Shift+Home}", "{ctrl-shift-home}");
+        }
+
+        [Test]
+        public void Test4()
+        {
+            TestSingle("{Shift+ExtEnd}", "{shift-end}");
+        }
+
+        [Test]
+        public void Test100()
+        {
+            Test("git branch -vv{Enter}", "git branch -vv{enter}");
+        }
+
+        [Test]
+        public void Test101()
+        {
+            Test("{Ctrl+Shift+Home}git branch -vv{Enter}", "{ctrl-shift-home}git branch -vv{enter}");
+        }
+
+        private void Test(string ddKeys, string expectedTalonKeys)
+        {
+            string actual = KeysConverter.Convert(ddKeys);
+            Assert.That(actual, Is.EqualTo(expectedTalonKeys));
+        }
+
+        private void TestSingle(string ddKeys, string expectedTalonKeys)
+        {
+            string actual = KeysConverter.ConvertSingleKey(ddKeys);
+            Assert.That(actual, Is.EqualTo(expectedTalonKeys));
+        }
+    }
+}
