79239a97ead0259cf193be441ee59bc4e7b8d44a
ronzulu <75528127+ronzulu@users.noreply.github.com>
2024-10-04 10:34:30 +1000

    support numbers between 9 and 9999

--- dragon-dictate/ConvertDict/ConvertDict/WordConverter.cs
+++ dragon-dictate/ConvertDict/ConvertDict/WordConverter.cs
@@ -24,22 +24,81 @@ namespace ConvertDict
 
         private static string ConvertStringToTalon(string str)
         {
-            return (str.Length == 1) ? ConvertCharToTalon(str[0]) : str;
+            string result;
+            if (int.TryParse(str, out var v))
+                result = ConvertNumberToTalon(v);
+            else 
+                result = str;
+            return result;
         }
 
-        private static string ConvertCharToTalon(char ch) => ch switch
+        private static string ConvertNumberToTalon(int num)
         {
-            '0' => "zero",
-            '1' => "one",
-            '2' => "two",
-            '3' => "three",
-            '4' => "four",
-            '5' => "five",
-            '6' => "six",
-            '7' => "seven",
-            '8' => "eight",
-            '9' => "nine",
-            _ => ch.ToString()
+            string result;
+            if ((num >= 0) && (num <= 19))
+                result = ConvertSmallNumberToTalon(num);
+            else if (num <= 99)
+            {
+                int tens = (num / 10) * 10;
+                string tensStr = tens switch
+                {
+                    20 => "twenty",
+                    30 => "thirty",
+                    40 => "forty",
+                    50 => "fifty",
+                    60 => "sixty",
+                    70 => "seventy",
+                    80 => "eighty",
+                    90 => "ninety",
+                    _ => throw new Exception()
+                };
+                result = tensStr;
+                int digit = num % 10;
+                if (digit > 0)
+                    result += " " + ConvertSmallNumberToTalon(digit);
+            }
+            else if (num <= 9999)
+            {
+                int hundreds = (num / 100);
+                int mod = (num % 100);
+                result = ConvertNumberToTalon(hundreds);
+                result += (hundreds < 10) ? " hundred" : " [hundred]";
+                if (mod > 0)
+                {
+                    if (mod <= 9)
+                        result += " [zero]";
+                    result += $" {ConvertNumberToTalon(mod)}";
+                }
+            }
+            else
+                result = num.ToString();
+
+            return result;
+        }
+
+        private static string ConvertSmallNumberToTalon(int num) => num switch
+        {
+            0 => "zero",
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+            10 => "ten",
+            11 => "eleven",
+            12 => "twelve",
+            13 => "thirteen",
+            14 => "fourteen",
+            15 => "fifteen",
+            16 => "sixteen",
+            17 => "seventeen",
+            18 => "eighteen",
+            19 => "nineteen",
+            _ => throw new Exception()
         };
     }
 }
---
+++ dragon-dictate/ConvertDict/TestProject/WordConverterTest.cs
@@ -0,0 +1,67 @@
+﻿using ConvertDict;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class WordConverterTest
+    {
+        [Test]
+        public void Test001()
+        {
+            Test(5, "five");
+        }
+
+        [Test]
+        public void Test002()
+        {
+            Test(15, "fifteen");
+        }
+
+        [Test]
+        public void Test003()
+        {
+            Test(20, "twenty");
+        }
+
+        [Test]
+        public void Test004()
+        {
+            Test(27, "twenty seven");
+        }
+
+        [Test]
+        public void Test005()
+        {
+            Test(100, "one hundred");
+        }
+
+        [Test]
+        public void Test006()
+        {
+            Test(155, "one hundred fifty five");
+        }
+
+        [Test]
+        public void Test007()
+        {
+            Test(305, "three hundred [zero] five");
+        }
+
+        [Test]
+        public void Test008()
+        {
+            Test(2024, "twenty [hundred] twenty four");
+        }
+
+        public void Test(int v, string expectedStr)
+        {
+            string word = "[" + v.ToString() + "]";
+            string? actual = WordConverter.ConvertDragonWordToTalonRule(word);
+            Assert.That(actual, Is.EqualTo(expectedStr));
+        }
+    }
+}
